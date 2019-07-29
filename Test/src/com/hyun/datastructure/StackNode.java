9a5f5488463dabafbde8fa498b0bd549828b311e
SeungHyunBaek <back947@naver.com>
2019-07-29 16:33:36 +0900

    Delete Test Project

--- Test/src/com/hyun/datastructure/StackNode.java
+++
@@ -1,7 +0,0 @@
-package com.hyun.datastructure;
-
-public class StackNode {
-	// char형 데이터를 담을 때를 기준으로 만든 노드
-	char item;
-	StackNode next;
-}
--- Test/src/com/hyun/datastructure/StackNode2.java
+++
@@ -1,7 +0,0 @@
-package com.hyun.datastructure;
-
-public class StackNode2 {
-	// char형 데이터를 담을 때를 기준으로 만든 노드
-	int item;
-	StackNode2 next;
-}
